a4278d0f5cbb2bbc26450b7d2aa343d2e048ef48
Miurionochek <ivanfedoniukwork@gmail.com>
2024-04-25 20:42:16 +0300

    create userWorkTable window| map userCards

--- Frontend/hackaton/src/pages/UserWorkTable/UserWorkTable.tsx
+++ Frontend/hackaton/src/pages/UserWorkTable/UserWorkTable.tsx
@@ -1,7 +1,38 @@
-import { Box } from "@mui/material";
+import { Box, Button } from "@mui/material";
 
 import styles from "./UserWorkTable.module.scss";
+import { UserBasedCardsData } from "../../interfaces/UserInterfaces";
+import RequestCard from "../../components/RequestCard/RequestCard";
+
+const testData: UserBasedCardsData[] = [
+  {
+    id: 1,
+    title: "Drones_1",
+    description: "We are making drones and need money for details.",
+  },
+  {
+    id: 2,
+    title: "Drones_2",
+    description: "We are making drones and need money for details.",
+  },
+];
 
 export default function UserWorkTable() {
-  return <Box className={styles.Box}>Bla Bla</Box>;
+  return (
+    <Box className={styles.Box}>
+      <Box className={styles.Button_Container}>
+        <Button variant="contained">Create Request</Button>
+      </Box>
+      <Box>
+        {testData.map((card) => (
+          <RequestCard
+            key={card.id}
+            id={card.id}
+            title={card.title}
+            description={card.description}
+          />
+        ))}
+      </Box>
+    </Box>
+  );
 }
