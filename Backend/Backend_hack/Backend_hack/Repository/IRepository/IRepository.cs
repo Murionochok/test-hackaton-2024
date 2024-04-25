50095da9b513e51211eb609a69645d33ecb9260f
Ravineko <jaroslavbychkov2004@gmail.com>
2024-04-25 12:57:13 +0300

    implement base repository

---
+++ Backend/Backend_hack/Backend_hack/Repository/IRepository/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq.Expressions;
+
+namespace Backend_hack.Repository.IRepository
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<List<T>> GetAllAsync(string sessionToken, Expression<Func<T, bool>>? filter = null, string? includeProperties = null);
+
+        Task<T> GetAsync(Expression<Func<T, bool>> filter = null, bool tracked = true, string? includeProperties = null);
+        Task CreateAsync(T entity);
+        Task DeleteAsync(T entity, string token);
+        Task SaveAsync();
+    }
+}
