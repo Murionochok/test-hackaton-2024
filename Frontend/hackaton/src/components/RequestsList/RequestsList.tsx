b9d59f8d50236cc886a92b588f7d882d71bdc144
Dmytro-Skorokhodov <skorokhodovdmytro@gmail.com>
2024-04-28 21:55:00 +0300

    added fetch requests (partially)

--- Frontend/hackaton/src/components/RequestsList/RequestsList.tsx
+++ Frontend/hackaton/src/components/RequestsList/RequestsList.tsx
@@ -2,14 +2,16 @@ import { Box } from "@mui/material";
 import { testData } from "../../pages/UserWorkTable/UserWorkTable";
 import RequestCard from "../RequestCard/RequestCard";
 import { Link } from "react-router-dom";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { ThunkDispatch, AnyAction } from "@reduxjs/toolkit";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import {
   UserCreateRequestData,
   UserRequestData,
 } from "../../interfaces/UserInterfaces";
 import { fetchUserRequests } from "../../store/request/request-slice";
+import { ReduxInterface } from "../../store";
+import Loader from "../UI/Loader";
 
 interface linkPath {
   linkPath: string;
@@ -17,29 +19,48 @@ interface linkPath {
 export default function RequestsView({ linkPath }: linkPath) {
   const dispatchReq: ThunkDispatch<UserRequestData, undefined, AnyAction> =
     useDispatch();
+  const [requests, setRequests] = useState([{}]);
+
+  const fetching = useSelector(
+    (state: ReduxInterface) => state.requests.fetching
+  );
+  const error = useSelector((state: ReduxInterface) => state.requests.error);
 
   useEffect(() => {
-    dispatchReq(fetchUserRequests())
+    const getRequests = async () => {
+      const userId = String(localStorage.getItem("USER_ID"));
+      const response = await dispatchReq(fetchUserRequests(userId));
+      if (response) {
+        // setRequests()
+      }
+      console.log(response);
+    };
+
+    getRequests();
   }, []);
   return (
-    <Box>
-      {testData.map((card) => (
-        <Link key={card.id} to={`${linkPath}${card.id}`}>
-          <RequestCard
-            id={card.id}
-            name={card.name}
-            surname={card.surname}
-            email={card.email}
-            phone={card.phone}
-            title={card.title}
-            address={card.address}
-            date={card.date}
-            tag={card.tag}
-            description={card.description}
-            state={card.state}
-          />
-        </Link>
-      ))}
-    </Box>
+    <>
+      <Loader open={fetching} />
+      {fetching ? "" : error ? <Box>{error}</Box> : ""}
+      <Box>
+        {testData.map((card) => (
+          <Link key={card.id} to={`${linkPath}${card.id}`}>
+            <RequestCard
+              id={card.id}
+              name={card.name}
+              surname={card.surname}
+              email={card.email}
+              phone={card.phone}
+              title={card.title}
+              address={card.address}
+              date={card.date}
+              tag={card.tag}
+              description={card.description}
+              state={card.state}
+            />
+          </Link>
+        ))}
+      </Box>
+    </>
   );
 }
